0a5b875104b6bed42042b376e0ccc5d6ec19133d
Sridhar2604 <sridharpuppy.26@gmail.com>
2020-03-06 23:27:14 +0530

    Script Added

--- src/main/java/Testcase.java
+++ src/main/java/Testcase.java
@@ -3,7 +3,7 @@ public class Testcase {
 
 	public static void main(String[] args) {
 		System.out.println("Print A");
-
-	}
-
+		System.out.println("Print B");
+		System.out.println("Print C");
+		}
 }
